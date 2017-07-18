517b285109126f30e11a142ced54b574587b1afd
Layl <LaylConway@users.noreply.github.com>
2017-07-18 10:33:26 +0200

    Add support for AdvancedWindow

--- src/lib.rs
+++ src/lib.rs
@@ -11,21 +11,25 @@ use std::collections::{VecDeque};
 use vulkano::swapchain::{Surface};
 use vulkano::instance::{Instance, InstanceExtensions};
 use vulkano_win::{VkSurfaceBuild, Window as VulkanoWinWindow};
-use winit::{EventsLoop, WindowBuilder, Event as WinitEvent, WindowEvent, ElementState, MouseButton as WinitMouseButton, KeyboardInput, MouseScrollDelta};
+use winit::{EventsLoop, WindowBuilder, Event as WinitEvent, WindowEvent, ElementState, MouseButton as WinitMouseButton, KeyboardInput, MouseScrollDelta, CursorState};
 use input::{Input, EventId, CloseArgs, Motion, Button, MouseButton, Key};
-use window::{Window, Size, WindowSettings};
+use window::{Window, Size, WindowSettings, Position, AdvancedWindow};
 
 pub fn required_extensions() -> InstanceExtensions {
     vulkano_win::required_extensions()
 }
 
 pub struct WinitWindow {
+    // TODO: These public fields should be changed to accessors
     pub window: VulkanoWinWindow,
     pub events_loop: EventsLoop,
     pub surface: Arc<Surface>,
 
     should_close: bool,
     queued_events: VecDeque<Input>,
+
+    title: String,
+    capture_cursor: bool,
 }
 
 impl WinitWindow {
@@ -46,6 +50,9 @@ impl WinitWindow {
 
             should_close: false,
             queued_events: VecDeque::new(),
+
+            title: settings.get_title(),
+            capture_cursor: false,
         }
     }
 }
@@ -80,13 +87,23 @@ impl Window for WinitWindow {
     }
 
     fn poll_event(&mut self) -> Option<Input> {
+        let mut center = self.window.window().get_inner_size().unwrap_or((2, 2));
+        center.0 /= 2;
+        center.1 /= 2;
+
         // Add all events we got to the event queue, since winit only allows us to get all pending
         //  events at once.
         {
             let queued_events = &mut self.queued_events;
+            let capture_cursor = self.capture_cursor;
             self.events_loop.poll_events(|event| {
-                push_events_for(event, queued_events)
+                push_events_for(event, queued_events, capture_cursor, center)
             });
+        }
+
+        // Center-lock the cursor if we're using capture_cursor
+        if self.capture_cursor {
+            self.window.window().set_cursor_position(center.0 as i32, center.1 as i32).unwrap();
         }
 
         // Get the first event in the queue
@@ -106,7 +123,60 @@ impl Window for WinitWindow {
     }
 }
 
-fn push_events_for(event: WinitEvent, queue: &mut VecDeque<Input>) {
+impl AdvancedWindow for WinitWindow {
+    fn get_title(&self) -> String {
+        self.title.clone()
+    }
+
+    fn set_title(&mut self, value: String) {
+        self.window.window().set_title(&value);
+        self.title = value;
+    }
+
+    fn get_exit_on_esc(&self) -> bool {
+        false
+    }
+
+    fn set_exit_on_esc(&mut self, _value: bool) {
+        // TODO: Implement this
+    }
+
+    fn set_capture_cursor(&mut self, value: bool) {
+        // If we're already doing this, just don't do anything
+        if value == self.capture_cursor {
+            return;
+        }
+
+        if value {
+            self.window.window().set_cursor_state(CursorState::Grab).unwrap();
+        } else {
+            self.window.window().set_cursor_state(CursorState::Normal).unwrap();
+        }
+        self.capture_cursor = value;
+    }
+
+    fn show(&mut self) {
+        self.window.window().show();
+    }
+
+    fn hide(&mut self) {
+        self.window.window().hide();
+    }
+
+    fn get_position(&self) -> Option<Position> {
+        self.window.window().get_position().map(|p| Position { x: p.0, y: p.1 })
+    }
+
+    fn set_position<P: Into<Position>>(&mut self, val: P) {
+        let val = val.into();
+        self.window.window().set_position(val.x, val.y)
+    }
+}
+
+fn push_events_for(
+    event: WinitEvent, queue: &mut VecDeque<Input>,
+    capture_cursor: bool, center: (u32, u32),
+) {
     let unsupported_input = Input::Custom(EventId("Unsupported Winit Event"), Arc::new(0));
 
     let event = match event {
@@ -135,8 +205,16 @@ fn push_events_for(event: WinitEvent, queue: &mut VecDeque<Input>) {
                 WindowEvent::KeyboardInput { device_id: _, input } => {
                     map_keyboard_input(&input)
                 },
-                WindowEvent::MouseMoved { device_id: _, position } =>
-                    Input::Move(Motion::MouseCursor(position.0, position.1)),
+                WindowEvent::MouseMoved { device_id: _, position } => {
+                    // Skip if the position is at the center and we have capture_cursor, this
+                    //  probably is from cursor center lock, or irrelevant.
+                    if capture_cursor &&
+                       position.0 as u32 == center.0 && position.1 as u32 == center.1 {
+                        return;
+                    }
+
+                    Input::Move(Motion::MouseCursor(position.0, position.1))
+                },
                 WindowEvent::MouseEntered { device_id: _ } => Input::Cursor(true),
                 WindowEvent::MouseLeft { device_id: _ } => Input::Cursor(false),
                 WindowEvent::MouseWheel { device_id: _, delta, phase: _ } => {
