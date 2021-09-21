7c56aa40b82cd91bc60bced2486d14f2e75a5afd
Kuroki Keiichiro <shinmili@users.noreply.github.com>
2021-09-21 14:13:43 +0900

    Simplify plain_winit_window example
    Following other `pistoncore-window` implementations

--- examples/plain_winit_window.rs
+++ examples/plain_winit_window.rs
@@ -1,17 +1,9 @@
 extern crate window;
 extern crate winit_window;
 
-use window::{AdvancedWindow, Window, WindowSettings};
+use window::WindowSettings;
 use winit_window::WinitWindow;
 
 fn main() {
-    let mut window = WinitWindow::new(&WindowSettings::new("Winit Window", (640, 480)).exit_on_esc(true));
-    window.show();
-
-    while !window.should_close() {
-        let e = match window.poll_event() {
-            Some(e) => e,
-            None => continue,
-        };
-    }
+    let _ = WinitWindow::new(&WindowSettings::new("Winit Window", (640, 480)));
 }
